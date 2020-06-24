39682072b76771935bb1f8c23ea193151bb02b92
Me-SAM <abhay4vardhan@gmail.com>
2020-06-24 21:41:13 +0530

    Updates

---
+++ precache-manifest.46038f4620c778e817060f18ba88ee33.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c19b3684af49453ede5e7f1cde40b56a",
+    "url": "/lightsout/index.html"
+  },
+  {
+    "revision": "0d44ce5be9f111a9a3e9",
+    "url": "/lightsout/static/css/main.b13ccd03.chunk.css"
+  },
+  {
+    "revision": "6d7adb45fb0b810cdb3b",
+    "url": "/lightsout/static/js/2.6d727044.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/lightsout/static/js/2.6d727044.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d44ce5be9f111a9a3e9",
+    "url": "/lightsout/static/js/main.7ddbd212.chunk.js"
+  },
+  {
+    "revision": "329d7329564d52853507",
+    "url": "/lightsout/static/js/runtime-main.34638c66.js"
+  }
+]);
